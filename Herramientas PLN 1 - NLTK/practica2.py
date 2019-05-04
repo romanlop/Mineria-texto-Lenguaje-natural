2350530a02313e5053d1d817994fbdc9e2c3226f
Roman <Ruman@iMac-de-Roman.local>
2019-05-04 21:15:50 +0200

    v.01.00

--- Herramientas PLN 1 - NLTK/practica2.py
+++ Herramientas PLN 1 - NLTK/practica2.py
@@ -16,14 +16,14 @@ sents = cess_esp.tagged_sents() #Devuelve cada palabra con su tag correspondient
 
 # 3. Creamos un conjunto de entrenamiento y otro de prueba
 #Metemos en el conjunto de entrenamiento el 90% de las frases, y el restante 10% en el conjunto de test
-"""training = []
+training = []
 test = []
 for i in range(len(sents)):
     if i % 10:
         training.append(sents[i])
     else:
         test.append(sents[i])
-"""
+
        
     
 # 4. Creamos cuatro tipos distintos de analizadores morfológicos: 
@@ -31,21 +31,36 @@ for i in range(len(sents)):
 # - Otro basadoen bigramas: aprende de la estadística de una palabra y su palabra anterior
 # - Otro basado en trigramas: aprende a taggear una palabra basandose en la estadistica de la palabra y sus 2 anteriores
 # - Otro basado en Modelos Ocultos de Markov (en inglés Hidden Markov Models, HMM): es el modelo mas completo
-"""
-unigram_tagger = UnigramTagger(sents)
+
+unigram_tagger = UnigramTagger(training)
 bigram_tagger = BigramTagger(training, backoff=unigram_tagger)
 trigram_tagger = TrigramTagger(training, backoff=unigram_tagger)
 hmm_tagger = HiddenMarkovModelTagger.train(training)
-"""
+
 
 # 5. Evaluamos sobre el conjunto de test que no usamos para el entrenamiento, para ver qué porcentaje de acierto hemos conseguido
 print ('Acierto con unigramas:',unigram_tagger.evaluate(test)*100)
 print ('Acierto con bigramas:',bigram_tagger.evaluate(test)*100)
 print ('Acierto con trigramas:',trigram_tagger.evaluate(test)*100)
-#print ('Acierto con HMMs:',hmm_tagger.evaluate(test)*100)
+print ('Acierto con HMMs:',hmm_tagger.evaluate(test)*100)
 
 # 6. Vamos a probar algunos textos descargados de internet -> http://delenguayliteratura.com/Analisis_morfologico_I_ejercicios_resueltos.html
 texto = "Sombra tuya he de ser. ¿Por qué no está en la cárcel ese infame. A Martirio, aunque es enamoradiza , se le olvidará esto. Estuvo mucho tiempo detrás de ti y le gustabas . Nada lo ha podido evitar . Veo que todo es una terrible repetición."
 tokens = nltk.word_tokenize(texto)
 tagged = unigram_tagger.tag(tokens)
 print(tagged)
+
+
+#Vamos a probar como funciona con palabras que no existen en el Corpus.
+texto = "Los perros son buenos chuchetes."
+tokens = nltk.word_tokenize(texto)
+print ("TAGGER UNIGRAMA:",tagged)
+print ("________________________")
+tagged = bigram_tagger.tag(tokens)
+print ("TAGGER BIGRAMA:",tagged)
+print ("________________________")
+tagged = trigram_tagger.tag(tokens)
+print ("TAGGER TRIGRAMA:",tagged)
+print ("________________________")
+tagged = hmm_tagger.tag(tokens)
+print ("TAGGER HMMs:",tagged)   #Este si es capaz de identificarla de forma adecuada.
--- Herramientas PLN 1 - NLTK/practica2_extension.py
+++ Herramientas PLN 1 - NLTK/practica2_extension.py
@@ -4,5 +4,55 @@
 Created on Sat May  4 20:18:02 2019
 
 @author: Ruman
+Para esta práctica es importante leer el apartado 5 del libro de NLTK -> http://www.nltk.org/book/ch05.html
 """
 
+import nltk
+from nltk.corpus import cess_cat 
+from nltk.tag import UnigramTagger, BigramTagger, TrigramTagger, DefaultTagger
+from nltk.tag.hmm import HiddenMarkovModelTagger
+
+
+sents = cess_cat.tagged_sents() 
+
+"""
+training = []
+test = []
+for i in range(len(sents)):
+    if i % 10:
+        training.append(sents[i])
+    else:
+        test.append(sents[i])
+
+#Aunque vamos a tratar de centranos en trigram los entrenamos todos.
+unigram_tagger = UnigramTagger(training)
+bigram_tagger = BigramTagger(training, backoff=unigram_tagger)
+trigram_tagger = TrigramTagger(training, backoff=unigram_tagger)
+"""
+
+#Evaluamos sobre el conjunto de test que no usamos para el entrenamiento, para ver qué porcentaje de acierto hemos conseguido
+print ('Acierto con unigramas:',unigram_tagger.evaluate(test)*100)
+print ('Acierto con bigramas:',bigram_tagger.evaluate(test)*100)
+print ('Acierto con trigramas:',trigram_tagger.evaluate(test)*100)
+
+
+#Vamos a trabajar con una frase en catalufo:
+frase_cat = "el president de la Generalitat ha tingut 4 chuchetes"
+tokens = nltk.word_tokenize(frase_cat)
+tagged = trigram_tagger.tag(tokens)
+print ("TAGGER UNIGRAMA CAT:",tagged)
+
+#Esta es la forma de aplicar varios taggeadores -> Backoff
+#Trabajamos con los datos de entrenamiento 'training_cat' que suponen el 90% del corpus
+default_tagger = DefaultTagger ('NLTK_FASHION')
+unigram_tagger = UnigramTagger(training, backoff=default_tagger)
+bigram_tagger = BigramTagger(training, backoff=unigram_tagger)
+trigram_tagger = TrigramTagger(training, backoff=bigram_tagger)
+#Entrenamos este modelo
+print ('Acierto con unigramas:',unigram_tagger.evaluate(test)*100)
+print ('Acierto con bigramas:',bigram_tagger.evaluate(test)*100)
+print ('Acierto con trigramas:',trigram_tagger.evaluate(test)*100)
+
+#Lo ejecutamos sobre nuestro palabro
+tagged = trigram_tagger.tag(tokens)
+print ("TAGGER UNIGRAMA CAT:",tagged)
