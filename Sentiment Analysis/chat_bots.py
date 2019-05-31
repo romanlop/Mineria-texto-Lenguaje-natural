44c3452ee88cf3ed854f21e5851ce401864ad39c
Roman <Ruman@iMac-de-Roman.local>
2019-05-31 18:35:07 +0200

    v.01.00

--- Sentiment Analysis/chat_bots.py
+++ Sentiment Analysis/chat_bots.py
@@ -6,5 +6,73 @@ Created on Fri May 31 14:40:23 2019
 @author: Ruman
 """
 
-from nltk.chat.eliza import eliza_chat
-eliza_chat()
+from __future__ import print_function
+from nltk.chat.util import Chat, reflections
+
+# a table of response pairs, where each pair consists of a
+# regular expression, and a list of possible responses,
+# with group-macros labelled as %1, %2.
+
+pairs = (
+    
+    (
+        r'(.*) incidencia (.*)',
+        (
+            "Con que proyecto está relacionada la incidencia?",
+            "De que proyecto me está hablando?",
+        ),
+    ),    
+    (
+        r'incidencia (.*)',
+        (
+            "Con que proyecto está relacionada la incidencia?",
+            "De que proyecto me está hablando?",
+        ),
+    ),           
+    (
+        r'(.*)incidencia',
+        (
+            "Con que proyecto está relacionada la incidencia?",
+            "De que proyecto me está hablando?",
+        ),
+    ),      
+        
+    (
+        r'(.*)',
+        (
+            "Lo siento, no comprendo lo que quiere decir. ¿Podría tratar de explicarmelo con otras palabras?",
+            "Disculpe, no entiendo como puedo aydarle",
+        ),
+    ),
+)
+        
+reflections = {
+  "yo soy"       : "tu eres",
+  "yo era"      : "tu eras",
+  "yo"          : "tu",
+  "mio"        : "tuyo",
+  "yo seré"        : "tu serás",
+  "yo tengo"       : "tu tienes",
+  "mi"         : "tu",
+  "mio"        : "tuyo",
+  "tu eres"    : "Yo soy",
+  "tu eras"   : "Yo era",
+  "tu tienes"     : "Yo tengo",
+  "Tu serás"     : "Yo seré",
+  "Tuyo"       : "mio",
+  "Tus"      : "mis",
+  "Tu"        : "yo"
+}        
+
+
+
+def eliza_chat():
+    print("Bienvenido a RLS consuting\n---------")
+    print('En que podemos ayudarle? Escriba "quit" para salir.')
+    print('=' * 72)
+    eliza_chatbot = Chat(pairs, reflections)
+    eliza_chatbot.converse()
+
+if __name__ == "__main__":
+    eliza_chat()
+
